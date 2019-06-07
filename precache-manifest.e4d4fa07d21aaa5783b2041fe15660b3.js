bda994af69cca7a665f3eb4b44c0a7901ccd908a
Liam <lon.liam.li@gmail.com>
2019-06-06 17:31:38 -0700

    website rework to react

---
+++ precache-manifest.e4d4fa07d21aaa5783b2041fe15660b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfd6d70c70374ec3acf0b958702ecefa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b99a844ff2330814f9bf",
+    "url": "/static/css/main.0f26529b.chunk.css"
+  },
+  {
+    "revision": "f339c188813c7d97ff88",
+    "url": "/static/js/2.3b664295.chunk.js"
+  },
+  {
+    "revision": "b99a844ff2330814f9bf",
+    "url": "/static/js/main.940cf95f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
